4095512f7b24c7985e7171d693d59dc92cbde0dc
Kishlay Kumar <krishraj2408@gmail.com>
2025-08-18 23:06:36 +0530

    Created profile view with Jwtauthentication

--- users/views.py
+++ users/views.py
@@ -2,10 +2,27 @@ from django.shortcuts import render
 from rest_framework import generics
 from django.contrib.auth.models import User
 from .serializers import RegisterSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
 
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
 
-# Create your views here.
+    def get(self, request):
+        user = request.user
+        return Response({
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+            "is_staff": user.is_staff,   
+        })
+
+
+
+
+
