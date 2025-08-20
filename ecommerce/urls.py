5b69aa619c31d1c6063d556d2a18474440818def
Kishlay Kumar <krishraj2408@gmail.com>
2025-08-20 23:13:55 +0530

    made changes in setting to make docker work

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +25,4 @@ urlpatterns = [
     path("api/",include("Shop.urls")),
     path("api/",include("Cart.urls")),
     path("api/",include("Order.urls"))
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
